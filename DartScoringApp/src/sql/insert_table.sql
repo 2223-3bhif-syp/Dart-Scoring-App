b1ab69818996ebbe8c1b38874d04abdeee04d43f
Nikolaus-Alexander <a.nikolaus@HTBLALeonding.onmicrosoft.com>
2023-02-12 16:03:33 +0100

    Update insert_table.sql

--- DartScoringApp/src/sql/insert_table.sql
+++ DartScoringApp/src/sql/insert_table.sql
@@ -3,14 +3,13 @@ VALUES ('Franz', 100),
        ('Gustav', 200),
        ('Tim', 150);
 
--- Populate Game table
 INSERT INTO Game (PLAYER_AMOUNT, CURRENT_PLAYER)
 VALUES (3, 1),
        (2, 2),
        (4, 3);
 
--- Populate Gametype table
+
 INSERT INTO Gametype (GAME_TYPE)
-VALUES ('Action'),
-       ('Strategy'),
-       ('Sports');
+VALUES ('Endless'),
+       ('Fast'),
+       ('Normal');
