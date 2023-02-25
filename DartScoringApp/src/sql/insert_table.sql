006d6cec6d57bb2cff4a6a57d7714052e3d70d73
LukaCvijic <lukacvijic@outlook.com>
2023-02-25 20:01:27 +0100

    Fixed tables and inserted data.

--- DartScoringApp/src/sql/insert_table.sql
+++ DartScoringApp/src/sql/insert_table.sql
@@ -1,15 +1,14 @@
-INSERT INTO Player (NAME, POINTS)
+INSERT INTO DSA_Player (NAME, POINTS)
 VALUES ('Franz', 100),
        ('Gustav', 200),
        ('Tim', 150);
 
-INSERT INTO Game (PLAYER_AMOUNT, CURRENT_PLAYER)
+INSERT INTO DSA_Game (PLAYER_AMOUNT, CURRENT_PLAYER)
 VALUES (3, 1),
        (2, 2),
        (4, 3);
 
 
-INSERT INTO Gametype (GAME_TYPE)
-VALUES ('Endless'),
-       ('Fast'),
-       ('Normal');
+INSERT INTO DSA_Gametype (GAME_TYPE)
+VALUES ('301'),
+       ('501');
